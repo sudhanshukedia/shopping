30232bb603a074bc20cb07327089a77c7635b585
Saurabh kedia <sudhanshu@Saurabh-MacBook-Air.local>
2019-03-04 16:43:48 +0530

    Initial commit

---
+++ src/main/java/com/onlineShopping/shopping/ShoppingApplication.java
@@ -0,0 +1,15 @@
+package com.onlineShopping.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class ShoppingApplication {
+
+	public static void main(String[] args) {
+		run(ShoppingApplication.class, args);
+	}
+
+}
